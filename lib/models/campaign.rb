b94b9649a7feb1df63795dab6f8931e596f2af4e
lucas renan <contato@lucasrenan.com>
2018-03-18 18:10:47 +0100

    Add validations to Campaign

--- lib/models/campaign.rb
+++ lib/models/campaign.rb
@@ -1,2 +1,6 @@
 class Campaign < ActiveRecord::Base
+  validates_presence_of :job_id
+  validates_presence_of :status
+  validates_presence_of :external_reference
+  validates_presence_of :ad_description
 end
--- spec/models/campaign_spec.rb
+++ spec/models/campaign_spec.rb
@@ -1,4 +1,8 @@
 require 'app_helper'
 
 RSpec.describe Campaign, type: :model do
+  it { is_expected.to validate_presence_of(:job_id) }
+  it { is_expected.to validate_presence_of(:status) }
+  it { is_expected.to validate_presence_of(:external_reference) }
+  it { is_expected.to validate_presence_of(:ad_description) }
 end
