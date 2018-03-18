8f49d8dffdf270bd971988958d414193c2ac43fe
lucas renan <contato@lucasrenan.com>
2018-03-18 16:19:12 +0100

    Configure RSpec

--- Gemfile
+++ Gemfile
@@ -4,3 +4,5 @@ gem 'faraday', '~> 0.14.0'
 gem 'faraday_middleware', '~> 0.12.2'
 gem 'pg', '~> 1.0.0'
 gem 'standalone_migrations', '~> 5.2.3'
+
+gem 'rspec', '~> 3.7.0'
--- lib/main.rb
+++ lib/main.rb
@@ -1,14 +1,2 @@
-require 'faraday'
-require 'faraday_middleware'
-
-url = 'https://mockbin.org/'
-connection = Faraday.new(url: url) do |faraday|
-  faraday.response :json
-
-  faraday.adapter Faraday.default_adapter
-end
-
-response = connection.get('bin/fcb30500-7b98-476f-810d-463a0b8fc3df')
-
-puts response.body
-
+require_relative '../db/connection'
+require_relative 'models/campaign'
